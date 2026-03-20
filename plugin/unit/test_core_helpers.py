d3ce807ca9b87425c54bbaff35494e3299dc4ff5
Daniele <dmasotti@gmail.com>
2026-03-20 15:56:01 +0100

    test(config): extend plugin store repair coverage

--- plugin/unit/test_core_helpers.py
+++ plugin/unit/test_core_helpers.py
@@ -65,9 +65,11 @@ update_calibre_isbn_if_required = core.update_calibre_isbn_if_required
 class ConfigHelpersTest(unittest.TestCase):
     def setUp(self):
         self.orig_store = dict(cfg.plugin_prefs[cfg.STORE_PLUGIN])
+        self.orig_core_store = dict(core.cfg.plugin_prefs[core.cfg.STORE_PLUGIN])
 
     def tearDown(self):
         cfg.plugin_prefs[cfg.STORE_PLUGIN] = self.orig_store
+        core.cfg.plugin_prefs[core.cfg.STORE_PLUGIN] = self.orig_core_store
 
     def test_remote_feature_toggle_removed(self):
         assert not hasattr(cfg, 'show_remote_features')
@@ -92,6 +94,60 @@ class ConfigHelpersTest(unittest.TestCase):
         cfg.plugin_prefs[cfg.STORE_PLUGIN][cfg.KEY_UPDATE_ISBN] = 'AUTO'
         update_calibre_isbn_if_required(book, '333', update_isbn='AUTO')
         self.assertEqual(book['calibre_isbn'], '333')
+
+    def test_http_helper_repairs_missing_dev_credentials_in_partial_store(self):
+        partial_store = {
+            core.cfg.KEY_REST_ENDPOINT: 'https://example.test/api',
+        }
+        cfg.plugin_prefs[cfg.STORE_PLUGIN] = dict(partial_store)
+        core.cfg.plugin_prefs[core.cfg.STORE_PLUGIN] = dict(partial_store)
+
+        helper = core.HttpHelper()
+
+        self.assertEqual(helper.devkey_token, core.cfg.DEFAULT_STORE_VALUES[core.cfg.KEY_DEV_TOKEN])
+        self.assertEqual(helper.devkey_secret, core.cfg.DEFAULT_STORE_VALUES[core.cfg.KEY_DEV_SECRET])
+        repaired_store = core.cfg.plugin_prefs[core.cfg.STORE_PLUGIN]
+        self.assertEqual(repaired_store[core.cfg.KEY_DEV_TOKEN], core.cfg.DEFAULT_STORE_VALUES[core.cfg.KEY_DEV_TOKEN])
+        self.assertEqual(repaired_store[core.cfg.KEY_DEV_SECRET], core.cfg.DEFAULT_STORE_VALUES[core.cfg.KEY_DEV_SECRET])
+
+    def test_get_plugin_store_repair_preserves_existing_values_and_backfills_defaults(self):
+        custom_store = {
+            cfg.KEY_REST_ENDPOINT: 'https://custom.example/api',
+            cfg.KEY_HTTP_TIMEOUT: 77,
+            cfg.KEY_SYNC_FILES_ENABLED: False,
+        }
+        cfg.plugin_prefs[cfg.STORE_PLUGIN] = dict(custom_store)
+
+        repaired = cfg.get_plugin_store(repair=True)
+
+        self.assertEqual(repaired[cfg.KEY_REST_ENDPOINT], 'https://custom.example/api')
+        self.assertEqual(repaired[cfg.KEY_HTTP_TIMEOUT], 77)
+        self.assertFalse(repaired[cfg.KEY_SYNC_FILES_ENABLED])
+        self.assertEqual(repaired[cfg.KEY_DEV_TOKEN], cfg.DEFAULT_STORE_VALUES[cfg.KEY_DEV_TOKEN])
+        self.assertEqual(repaired[cfg.KEY_DEV_SECRET], cfg.DEFAULT_STORE_VALUES[cfg.KEY_DEV_SECRET])
+        self.assertEqual(repaired[cfg.KEY_V5_CLIENT_BATCH_SIZE], cfg.DEFAULT_STORE_VALUES[cfg.KEY_V5_CLIENT_BATCH_SIZE])
+        self.assertEqual(repaired[cfg.KEY_DISCOVERY_CACHE_TTL], cfg.DEFAULT_STORE_VALUES[cfg.KEY_DISCOVERY_CACHE_TTL])
+
+    def test_get_plugin_store_repair_recovers_from_non_dict_store(self):
+        cfg.plugin_prefs[cfg.STORE_PLUGIN] = 'broken-store'
+
+        repaired = cfg.get_plugin_store(repair=True)
+
+        self.assertIsInstance(repaired, dict)
+        self.assertEqual(repaired[cfg.KEY_DEV_TOKEN], cfg.DEFAULT_STORE_VALUES[cfg.KEY_DEV_TOKEN])
+        self.assertEqual(repaired[cfg.KEY_REST_ENDPOINT], cfg.DEFAULT_STORE_VALUES[cfg.KEY_REST_ENDPOINT])
+        self.assertEqual(cfg.plugin_prefs[cfg.STORE_PLUGIN][cfg.KEY_DEV_SECRET], cfg.DEFAULT_STORE_VALUES[cfg.KEY_DEV_SECRET])
+
+    def test_get_plugin_store_without_repair_does_not_mutate_partial_store(self):
+        partial_store = {
+            cfg.KEY_REST_ENDPOINT: 'https://readonly.example/api',
+        }
+        cfg.plugin_prefs[cfg.STORE_PLUGIN] = dict(partial_store)
+
+        loaded = cfg.get_plugin_store(repair=False)
+
+        self.assertEqual(loaded, partial_store)
+        self.assertNotIn(cfg.KEY_DEV_TOKEN, cfg.plugin_prefs[cfg.STORE_PLUGIN])
 
 
 class SearchableAuthorTest(unittest.TestCase):
